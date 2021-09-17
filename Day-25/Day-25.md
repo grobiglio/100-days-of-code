87518cb789e2e0431928b41e79dea0a6979bb162
Guillermo A. Roboglio <grobiglio@gmail.com>
2021-09-16 23:40:26 -0300

    Day 25 complete

--- Day-25/Day-25.md
+++ Day-25/Day-25.md
@@ -1,4 +1,5 @@
 # Día 25 de _100 Days of Code_.
 
 Documentación de la librería [Pandas](https://pandas.pydata.org/docs/index.html).
+
 Documentación de la librería [Turtle Graphics](https://docs.python.org/3/library/turtle.html#module-turtle).
--- Day-25/us-states-game/main.py
+++ Day-25/us-states-game/main.py
@@ -1,17 +1,23 @@
 import turtle
 import pandas as pd
+from pandas.core.frame import DataFrame
 
 screen = turtle.Screen()
 screen.title("U.S. States Game")
-# Funciona solamente con ruta absoluta
 image = "C:/Users/Guillermo/Documents/curso-100-DoP/\
 ejercicios-100-days-of-code/Day-25/us-states-game/map.gif"
 screen.register_shape(image)
 turtle.shape(image)
 
+writer_turtle = turtle.Turtle()
+writer_turtle.hideturtle()
+writer_turtle.penup()
+
 states = pd.read_csv("C:/Users/Guillermo/Documents/curso-100-DoP/\
 ejercicios-100-days-of-code/Day-25/us-states-game/50_states.csv")
 states_list = states["state"].to_list()
+x_coord = states["x"].to_list()
+y_coord = states["y"].to_list()
 guessed_states = []
 
 while len(guessed_states) < 50:
@@ -23,18 +29,24 @@ while len(guessed_states) < 50:
     else:
         break
 
+    if answer_state == "Exit": break
+
     if (answer_state in states_list) and (answer_state not in guessed_states):
         guessed_states.append(answer_state)
-        turtle.setposition(100,100)
-        turtle.write(answer_state, align="center", font=("Arial", 8, "normal"))
+        writer_turtle.setposition(x_coord[states_list.index(answer_state)],
+                                  y_coord[states_list.index(answer_state)])
+        writer_turtle.write(answer_state, align="center", font=("Arial", 8, "normal"))
     else:
         print("Incorrecto.")
 
-print(guessed_states)
-# state_info = states[states["state"] == answer_state]
-# state_name = state_info["state"]
-# state_x_coord = state_info["x"]
-# state_y_coord = state_info["y"]
-# print(f"{state_name}\n {state_x_coord}\n{state_y_coord}")
+for guessed_state in guessed_states:
+    states_list.remove(guessed_state)
 
-screen.exitonclick()
+print(states_list)
+states_to_learn = DataFrame(
+    {"states": states_list}
+)
+states_to_learn.to_csv("C:/Users/Guillermo/Documents/curso-100-DoP/\
+ejercicios-100-days-of-code/Day-25/us-states-game/States-to-Learn.csv", index=False)
+
+# screen.exitonclick()
